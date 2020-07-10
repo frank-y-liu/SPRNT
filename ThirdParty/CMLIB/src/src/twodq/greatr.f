0801f9b856456029be56b0dbb49925f89205c878
cy4782 <yuchanway@utexas.edu>
2020-07-10 14:33:59 -0500

    Fread Method merge

--- ThirdParty/CMLIB/src/src/twodq/greatr.f
+++
@@ -1,6 +0,0 @@
-      LOGICAL FUNCTION GREATR(A,B,NWDS)
-      INTEGER NWDS
-      REAL A(NWDS), B(NWDS)
-      GREATR= A(1) .GT. B(1)
-      RETURN
-      END
--- ThirdParty/CMLIB/src/sun.stubs/uivp.f
+++
@@ -1,2 +0,0 @@
-      subroutine uivp
-      end
